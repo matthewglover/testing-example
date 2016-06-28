4c8a3b5449ae5824b8d9db0945e4dbb81437d93e
Matt Glover <matthewglover@me.com>
2016-06-28 15:07:42 +0100

    feat(coverage): remove coverage files

--- test/math-fns.test.js
+++ test/math-fns.test.js
@@ -5,3 +5,8 @@ tape('add', t => {
   t.equal(mathFns.add(10, 11), 21, 'adds two number');
   t.end();
 });
+
+tape('mult', t => {
+  t.equal(mathFns.mult(2, 2), 4, 'multiplies two numbers');
+  t.end();
+});
